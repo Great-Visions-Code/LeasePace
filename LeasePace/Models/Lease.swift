f89d8fcef4a389b3ee01abad6c25893ca18e7e4e
Gustavo Vazquez <great.visions@icloud.com>
2026-04-07 23:20:59 -0700

    Add Lease model struct
    Introduce Lease struct in LeasePace/Models/Lease.swift to represent lease details. Includes startDate, termMonths, milesAllowedPerYear, costPerMile, and currentMileage to support lease calculations and mileage tracking.

---
+++ LeasePace/Models/Lease.swift
@@ -0,0 +1,21 @@
+//
+//  Lease.swift
+//  LeasePace
+//
+//  Created by Great-Visions-Code on 4/1/26
+//
+
+import Foundation
+
+// Struct for Lease Details
+struct Lease {
+    var startDate: Date
+    
+    var termMonths: Int
+    
+    var milesAllowedPerYear: Int
+    
+    var costPerMile: Double
+    
+    var currentMileage: Double
+}
